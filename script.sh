20e6e67b0499775ec72e734b2789938c67615463
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-10-01 11:43:54 +0000

    Added exit 1 to all lines in script

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ STORAGE_POOL=$3
 # The new imported disk will get a name that depends on the current number of disks for the vm 
 DISK_COUNT=$4
 
-qm stop "$VM_ID"
-qm importdisk "$VM_ID" "$IMAGE_PATH" "$STORAGE_POOL"
-qm set "$VM_ID" --scsi0 "$STORAGE_POOL:vm-$VM_ID-disk-$DISK_COUNT"
-qm set "$VM_ID" --boot order=scsi0
-qm start "$VM_ID"
+qm stop "$VM_ID" || exit 1 
+qm importdisk "$VM_ID" "$IMAGE_PATH" "$STORAGE_POOL" || exit 1 
+qm set "$VM_ID" --scsi0 "$STORAGE_POOL:vm-$VM_ID-disk-$DISK_COUNT" || exit 1 
+qm set "$VM_ID" --boot order=scsi0 || exit 1 
+qm start "$VM_ID" || exit 1 
